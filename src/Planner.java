fae1f34ad8233df6c4064c39151a2f7687fc026e
danil732000@gmail.com <danil732000@gmail.com>
2020-06-05 13:13:53 +0400

    Revert "fix"
    This reverts commit 271132f5

--- src/Planner.java
+++ src/Planner.java
@@ -22,45 +22,46 @@ public class Planner {
     }
 
     public void start() {
+        int count = 0;
+        int indTh = 0;
         while (processes.size() != 0) {
-            int indTh = 0;
             for (int i = 0; i < processes.size(); i++) {
                 System.out.println("Выполняется " + processes.get(i).getDescription() + " QuantTime = " + processes.get(i).getQuantTime());
+                int quantTh = processes.get(i).getThread(indTh).quant();
                 while (processes.get(i).getCurrentTime() > 0) {
                     System.out.println(processes.get(i).getThread(indTh).getDescription() + " quant = " + processes.get(i).getThread(indTh).getQuantTime());
                     if (processes.get(i).getThread(indTh).getQuantTime() >= 0) {
                         processes.get(i).getThread(indTh).decreaseTime();
                         processes.get(i).decreaseCurrentTime();
-                        processes.get(i).getThread(indTh).decreaseCurrentTimeTh();
                         if (processes.get(i).getThread(indTh).getQuantTime() == 0) {
                             System.out.println(processes.get(i).getThread(indTh).getDescription() + " завершил свою работу");
-                            processes.get(i).getThread(indTh).restoreCurrentQuantTimeTh();
                             processes.get(i).deleteThread(indTh);
-                            processes.get(i);
-                            if (indTh >= processes.get(i).getCount()) {
-                                indTh = 0;
-                            }
+                            processes.get(i).setCount();
+                            count =0;
                         }
                     }
                     if (processes.get(i).isEmpty()) {
                         System.out.println(processes.get(i).getDescription() + " завершил свою работу");
                         processes.remove(i);
                         i--;
+                        count = 0;
                         break;
                     }
-                    if ((processes.get(i).getThread(indTh).getCurrentTimeTh() == 0)){
-                        processes.get(i).getThread(indTh).restoreCurrentQuantTimeTh();
+                    if (count == quantTh){
+                        count = 0;
                         indTh++;
                     }
                     if (indTh >= processes.get(i).getCount()) {
                         indTh = 0;
+                        count= 0;
                     }
                     if ((processes.get(i).getCurrentTime() == 0) && processes.get(i).getThread(indTh).getQuantTime() > 0) {
                         processes.get(i).restoreCurrentQuantTime();
-                        processes.get(i).getThread(indTh).restoreCurrentQuantTimeTh();
+                        count=0;
                         indTh = 0;
                         break;
                     }
+                    count++;
                 }
                 System.out.println();
             }
